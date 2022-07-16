03faadb58cc5c68bdd6f00a0f984cdd0a7c5e427
Olga Sirotinsky <olgasirotinsky@nada.local>
2022-07-16 14:39:36 -0700

    day 3 project

---
+++ D3-RotateNav/script.js
@@ -0,0 +1,9 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const cont = document.querySelector('.container');
+
+open.addEventListener('click', 
+() => cont.classList.add('show-nav'));
+
+close.addEventListener('click', 
+() => cont.classList.remove('show-nav'));
